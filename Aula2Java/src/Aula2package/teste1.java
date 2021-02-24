9d2346ee30defff87cdaefc2e6c706dde5d950c7
Tiago Alves <tsasa@iscte.iul.pt>
2021-02-24 19:56:40 +0000

    teste

---
+++ Aula2Java/src/Aula2package/teste1.java
@@ -0,0 +1,14 @@
+package Aula2package;
+
+public class teste1 {
+
+	public teste1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.print("Hello World!");
+
+	}
+
+}
